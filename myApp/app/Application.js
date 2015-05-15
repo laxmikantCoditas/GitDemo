f20d8c363bfb4496be66485ec800db9501cd52b2
Laxmikant Dudam <laxmikant@coditas.com>
2015-05-15 17:38:49 +0530

    applicaiton.js modified

--- myApp/app/Application.js
+++ myApp/app/Application.js
@@ -14,6 +14,7 @@ Ext.define('myApp.Application', {
     
     launch: function () {
         // TODO - Launch the application
+        // TODO - Launch the application
     },
 
     onAppUpdate: function () {
